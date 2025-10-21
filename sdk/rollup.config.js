9f44a718bfde9294de75ee2ba8d39d0407b562e4
christianscode1994 <fourieranalys@hotmail.com>
2025-10-21 23:32:44 +0300

    Update rollup.config.js

--- sdk/rollup.config.js
+++ sdk/rollup.config.js
@@ -1,20 +1,16 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import { terser } from "rollup-plugin-terser";
 
-export default [
-  {
-    input: "src/index.ts",
-    output: [
-      { file: "dist/index.esm.js", format: "es", sourcemap: false },
-      { file: "dist/index.cjs.js", format: "cjs", sourcemap: false }
-    ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json", declaration: true, rootDir: "src", outDir: "dist" }),
-      terser()
-    ]
-  }
-];
+export default {
+  input: "src/index.ts",
+  output: [
+    { file: "dist/index.esm.js", format: "es" },
+    { file: "dist/index.cjs.js", format: "cjs" }
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: "./tsconfig.json" })
+  ]
+};
